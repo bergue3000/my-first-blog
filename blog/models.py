45673019be73410d7e012a927e4db8561b8cdf7b
Leudembergue <bergue3000@hotmail.com>
2016-03-31 17:20:40 -0300

    adicionado o login de usuarios.

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
 
 	published_date = models.DateTimeField(blank=True, null=True)
 
-	def publish(self):
+	def published(self):
 		self.published_date = timezone.now()
 		self.save()
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+)
+MEDIA_ROOT = ''
+MEDIA_URL = '/media/'
+LOGIN_REDIRECT_URL = '/'
