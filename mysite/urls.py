49addc95268cea459b6bc9a8989fd5ab77463f56
Leudembergue <bergue3000@hotmail.com>
2016-04-03 19:46:01 -0300

    pequena correcao na alteracao de permissoes

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,4 +9,7 @@ urlpatterns = patterns('',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('blog.urls')),
+
 )
+
+
