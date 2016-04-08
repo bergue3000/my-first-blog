a5ad590ca6fe0f4cba71de91f3b04656b07f976f
Leudembergue <bergue3000@hotmail.com>
2016-04-08 18:00:35 -0300

    varias mudancas na media e css

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class DocumentForm(forms.Form):
+    docfile = forms.FileField(
+        label='Select a file',
+        help_text='max. 42 megabytes'
+    )
