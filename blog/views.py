d5e2c2a522963a08b56b7be85d52be750c2b0571
Leudembergue <bergue3000@hotmail.com>
2016-04-12 14:16:26 -0300

    adicionando imagens e alteracao de css

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import permission_required
 
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -32,7 +32,7 @@ def post_new(request):
 
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    posts = Post.objects.filter(published_date__isnull=True).order_by('published_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 #@login_required
